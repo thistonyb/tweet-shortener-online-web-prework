d94c5b2940be3c43bd268dead01d805fdbacdd3d
Tony Baker <this.tonybaker@gmail.com>
2019-02-21 19:29:20 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+def dictionary
+  {"hello" => 'hi', "to" => '2', "two" => '2', "too" => '2',
+    "for" => '4', "four" => '4', "be" => 'b', "you" => 'u',
+    "at" => "@", "and" => "&"
+  }
+end
