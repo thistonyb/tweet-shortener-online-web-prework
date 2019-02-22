2863bc8462b2b50f9ee361759f5044cc67fe1cea
Tony Baker <this.tonybaker@gmail.com>
2019-02-22 01:00:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,6 @@
 def dictionary
   {"hello" => 'hi', "to" => '2', "two" => '2', "too" => '2',
-    "for" => "4", "four" => '4', "be" => 'b', "you" => 'u',
+    "for" => '4', "four" => '4', "For" => '4', "be" => 'b', "you" => 'u',
     "at" => "@", "and" => "&"
   }
 end
